7a96b3eb1aa00a73828256d80866b8ba952c89bd
serendipit0502 <serendipit0502@163.com>
2018-09-01 16:28:35 +0800

    Umebrella framework is  a DynamicFramework

---
+++ UmbrellaApp_ DynamicFramework/UmbrellaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UmbrellaApp
+//
+//  Created by Serendipit on 2018/8/31.
+//  Copyright © 2018年 Serendipit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UmbrellaApp_ DynamicFramework/UmbrellaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UmbrellaApp
+//
+//  Created by Serendipit on 2018/8/31.
+//  Copyright © 2018年 Serendipit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
